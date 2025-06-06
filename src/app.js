76c548e464af828577176c9e2c13ae16ea33eb76
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-06-06 00:24:17 -0700

    PLaying with multiple route and route handlers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
-
-app.use("/",(req,res)=>{
-  res.send("You're in Home page")
-})
-
-app.use("/test", (req, res) => {
-  res.send("You're in Test page");
-});
-
-app.use("/h", (req, res) => {
-  res.send("You're in Hello page");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("In the first responce");
+    next();
+  },
+  (req, res) => {
+    console.log("In the second responce");
+    res.send("2nd responce");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is listening to port 7777 successfully");
