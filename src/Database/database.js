be2c42c8c74e1a93ec65d4f6b3b9be4e118dc394
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-07-23 23:56:38 +0500

    made changes in Schema

--- src/Database/database.js
+++ src/Database/database.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   await mongoose.connect(
     "mongodb+srv://rayyanahmedrak:MT9AJ1gsdkziv3gJ@rayyanahmed.he6ql4y.mongodb.net/devTinder"
   );
+  
 };
 
 module.exports = { connectDB };
--- src/app.js
+++ src/app.js
@@ -2,20 +2,43 @@ const express = require("express");
 const app = express();
 const { connectDB } = require("./Database/database");
 const User = require("./models/user");
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Rayyan",
-    lastname: "Ahmed",
-    EmailID: "Rayyanahmedrak@gmail.com",
-    Password: "Rayyanrak12+",
-    age: 21,
-  });
-
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User has been added successfully!");
   } catch {
     res.status(400).send("Error whle saving data" + err.message);
+  }
+});
+
+// getting user from database on the basis of email id
+app.get("/user", async (req, res) => {
+  const useremail = req.body.EmailID;
+
+  const users = await User.find({ EmailID: useremail });
+  try {
+    if (users === 0) {
+      res.status(404).send("user not found!");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(404).send("something went wrong!");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const users = await User.find({});
+  try {
+    if (users === 0) {
+      res.status(404).send("No data found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.send("Something went wrong!");
   }
 });
 connectDB()
