4ac7eb58ec12ce554b3bb6710a38466c2134cb33
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-07-24 23:25:23 +0500

    changes in some schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,18 +3,24 @@ const mongoose = require("mongoose");
 const userschema = new mongoose.Schema({
   firstName: {
     type: String,
+    unique: true,
+    minLength: 5,
+    maxlength: 50,
   },
   lastName: {
     type: String,
   },
   EmailID: {
     type: String,
+    unique: true,
   },
   Password: {
     type: String,
   },
   age: {
     type: Number,
+    min: 18,
+    max: 50,
   },
 });
 
